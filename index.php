ae64374444ec7804e7d0de86a81de5f5d867dafe
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-13 12:24:25 -0600

    Set up boiler plate, created the headings, basic layout.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en"
+<head>
+    <meta charset="UTF-8"/>
+    <title>Data Design for Science Daily References</title>
+</head>
+<body>
+    <h1>Data Design for Science Daily References</h1>
+    <h2>Persona</h2>
+        <p>Name: Lilly</p>
+    <h2>User Story</h2>
+        <p> As a....
+        </p>
+    <h2>Use Case</h2>
+        <p>Lilly checks Science Daily to see current science news in psychology.
+        </p>
+    <h2>Interaction Flow</h2>
+         <ol>
+            <li>Navigates to www.Sciencedaily.com</li>
+            <li>Navigates to health>mind and brain.</li>
+        </ol>
+</body>
+</html>
