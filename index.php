894dcd4e7da753d98fc7856f004112d144292875
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-14 10:32:00 -0600

    Added persona list, started interation flow, added hyperlinks to sciencedaily.com

--- index.php
+++ index.php
@@ -5,19 +5,26 @@
     <title>Data Design for Science Daily References</title>
 </head>
 <body>
-    <h1>Data Design for Science Daily References</h1>
+    <h1>Data Design for <a href="https://www.sciencedaily.com/"target="_blank">Science Daily</a> References</h1>
     <h2>Persona</h2>
-        <p>Name: Lilly</p>
+        <p><strong>Name</strong>: Lilly</p>
+        <p><strong>Age</strong>: 25</p>
+        <p><strong>Occupation</strong>: Undergraduate student of psychology</p>
+        <p><strong>Technology</strong>: Fluent user with a MacBook Pro 15" OS High Sierra 10.13.4 and an Apple iPhone X iOS 11.3</p>
+        <p><strong>Attitude</strong>: She is used to research and sometimes having to go several sources in to find what she wants, but hates getting lost in databases or the Google rabbit hole.</p>
+        <p><strong>Goal</strong>: To be able to find relevant articles and cite the sources to do additional research.</p>
+        <p><strong>Frustrations</strong>: Hates getting lost in databases trying to find original source material, or searching through Google results. Especially dislikes when article are cited incorrectly.  Inability to export or copy citations in APA format.</p>
     <h2>User Story</h2>
-        <p> As a....
-        </p>
+        <p> As a student, Lilly wants to use <a href="https://www.sciencedaily.com/"target="_blank">Science Daily</a> to find short news articles on current research.</p>
     <h2>Use Case</h2>
         <p>Lilly checks Science Daily to see current science news in psychology.
         </p>
     <h2>Interaction Flow</h2>
          <ol>
-            <li>Navigates to www.Sciencedaily.com</li>
-            <li>Navigates to health>mind and brain.</li>
+            <li>Navigates to <a href="https://www.sciencedaily.com/"target="_blank">Science Daily</a> </li>
+            <li>Mouses over "Health" in nav bar and clicks on "Mind and Brain"</li>
+            <li>Scrolls through the news articles populated on the "Mind and Brain" page</li>
+            <li></li>
         </ol>
 </body>
 </html>
