1fd4b8e41e4121f7f0454a6e0ec81b36548cd2c6
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-18 11:39:00 -0600

    created SQL table and ran it without errors

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,19 +1,32 @@
+ALTER DATABASE yjohnson6 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS reference;
+DROP TABLE IF EXISTS topic;
 CREATE TABLE topic (
     topicId BINARY(16) NOT NULL,
     topicName VARCHAR(16) NOT NULL,
-);
-CREATE TABLE article (
-    articleId BINARY(16) NOT NULL,
-    articlTopicId BINARY(16) NOT NULL,
-    articleAuthor VARCHAR(16) NOT NULL,
-    articleContent VARCHAR(65535)NOT NULL,
-    articleDate DATE(6) NOT NULL,
-    articleName VARCHAR(16) NOT NULL,
+    UNIQUE(topicId),
+    PRIMARY KEY(topicId)
 );
 CREATE TABLE reference (
     referenceId BINARY(16) NOT NULL,
-    referenceArticleId BINARY(16) NOT NULL,
     referenceAuthor VARCHAR(16) NOT NULL,
-    referenceDatePublished DATE,
+    referenceDatePublished DATE NOT NULL,
     referenceDoi VARCHAR (16),
+    UNIQUE (referenceId),
+    PRIMARY KEY (referenceId)
+);
+CREATE TABLE article (
+    articleId BINARY(16) NOT NULL,
+    articleTopicId BINARY(16) NOT NULL,
+    articleReferenceId BINARY(16) NOT NULL,
+    articleAuthor VARCHAR(16) NOT NULL,
+    articleContent BLOB NOT NULL,
+    articleDate DATE NOT NULL,
+    articleName VARCHAR(16) NOT NULL,
+    INDEX(articleTopicId),
+    INDEX(articleReferenceId),
+    FOREIGN KEY(articleTopicId) REFERENCES topic(topicId),
+    FOREIGN KEY(articleReferenceId) REFERENCES reference(referenceId),
+    PRIMARY KEY(articleId)
 );
