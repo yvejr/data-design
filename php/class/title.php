db07271136502df179ff98b7683c1b19aa8c2656
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-19 14:43:20 -0600

    correct docblock

--- php/class/title.php
+++ php/class/title.php
@@ -1,51 +1,77 @@
+<?php
 /**
-* list of topics by name and id
-* includes mutator and accessor for topic name and id
-**/
+ * list of topics by name and id
+ *
+ * includes mutator and accessor for topic name and id
+ **/
 
-class Topic {
+class Topic
+{
     //this is the name assigned to the topic
     private $topicName;
 
     //this is the id assigned to the topic
     private $topicId;
 
-    //mutator method for topic name
-    public function setTopicName($newTopicName) : void {
+    /**
+     * mutator method for topic name
+     **/
+    public function setTopicName($newTopicName): void
+    {
         $newTopicName = trim($newTopicName);
 
-    //verify the topic name is secure, if not then display error message
-    $newTopicName = filter_var($newTopicName FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-    if(empty($newTopicName) === true) {
-            throw(new \InvalidArgumentException("topic name is either empty or insecure"));
+        //verify the topic name is secure, if not then display error message
+        $newTopicName = filter_var($newTopicName FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    if (empty($newTopicName) === true) {
+        throw(new \InvalidArgumentException("topic name is either empty or insecure"));
     }
-    }
-
     //verify the topic name length will fit in the database
-    if(strlen($newTweetContent) > 16) {
-            throw(new \RangeException("title length too large; max of 16 characters"));
+    if (strlen($newTitleContent) > 16) {
+        throw(new \RangeException("title length too large; max of 16 characters"));
+    }
     }
 
-    //accessor for topic name
-    public function getTopicName() : string {
-        return($this->topicName);
+    /**
+     *accessor for topic name
+     **/
+    public function getTopicName(): string
+    {
+        return ($this->topicName);
     }
 
-    //mutator method for topic id
-    public function setTopicId($newTopicId) : void {
-        $newTopicId = trim($newTopicName);
+    /**
+     * mutator method for topic id
+     **/
+    public function setTopicId($newTopicId): void
+    {
+        {
+            $newTopicId = trim($newTopicName);
             try {
-                    $uuid = self::validateUuid($newTweetId);
-            } catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
-                    $exceptionType = get_class($exception);
-                    throw(new $exceptionType($exception->getMessage(), 0, $exception));
-            }
+                $uuid = self::validateUuid($newtitleId);
+            } catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+                newTopicName = filter_var($newTopicName FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    if (empty($newTopicName) === true) {
+        throw(new \InvalidArgumentException("topic name is either empty or insecure"));
+    }
+    }
 
-    // convert and store the tweet id
-    $this->tweetId = $uuid;
+            //verify the topic name length will fit in the database
+            if (strlen($newtitleContent) > 16) {
+                throw(new \RangeException("title length too large; max of 16 characters"));
+            }
+            /**
+             * accessor for topic name
+             **/
+            $exceptionType = get_class($exception);
+            throw(new $exceptionType($exception->getMessage(), 0, $exception));
         }
 
-    //accessor method for topic id
+        // convert and store the title id
+        $this->titleId = $uuid;
+    }
+/**
+ *accessor method for topic id
+ **/
     public function getTopicId() : Uuid{
             return($this->topicProfileId);
     }
