e6f60ffc99b33a9bb2999e3fd51b1e9a97f937a8
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-22 22:16:25 -0600

    this is a copy + paste from example then edit to fit my database needs.

--- php/class/title.php
+++ php/class/title.php
@@ -1,14 +1,13 @@
 <?php
-/**
- * list of topics by name and id
- *
- * includes mutator and accessor for topic name and id
- **/
+    /**
+     * list of topics by name and id
+     *
+     * includes mutator and accessor for topic name and id
+     **/
 
-class Topic
-{
+class Topic {
     //this is the name assigned to the topic
-    private $topicName;
+    public $topicName;
 
     //this is the id assigned to the topic
     private $topicId;
@@ -16,40 +15,43 @@ class Topic
     /**
      * mutator method for topic name
      **/
-    public function setTopicName($newTopicName): void {
+    public function setTopicName (string $newTopicName): void {
         $newTopicName = trim($newTopicName);
-
         //verify the topic name is secure, if not then display error message
-        $newTopicName = filter_var($newTopicName FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-    if (empty($newTopicName) === true) {
-        throw(new \InvalidArgumentException("topic name is either empty or insecure"));
-    }
-    //verify the topic name length will fit in the database
-    if (strlen($newTitleContent) > 16) {
-        throw(new \RangeException("title length too large; max of 16 characters"));
-    }
+        $newTopicName = filter_var($newTopicName);
+        FILTER_SANITIZE_STRING;
+        FILTER_FLAG_NO_ENCODE_QUOTES;
+        if (empty($newTopicName) === true) {
+            throw(new \InvalidArgumentException("topic name is either empty or insecure"));
+        }
+        //verify the topic name length will fit in the database
+        if (strlen($newTopicName) > 16) {
+            throw(new \RangeException("title length too large; max of 16 characters"));
+        }
     }
 
     /**
      *accessor for topic name
      **/
-    public function getTopicName(): string {
+    public function getTopicName(): string
+    {
         return ($this->topicName);
     }
 
     /**
      * mutator method for topic id
      **/
-    public function setTopicId($newTopicId): void {
-            $newTopicId = trim($newTopicName);
-            try {
-                $uuid = self::validateUuid($newtitleId);
-            }
-            catch {(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+    public function setTopicId($newTopicId): void
+    {
+        $newTopicId = trim($newTopicId);
+        try {
+            $uuid = self::validateUuid($newTopicId);
+        } catch {
+            (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
                 newTopicName = filter_var($newTopicName FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
                 if (empty($newTopicName) === true) {
-        throw(new \InvalidArgumentException("topic name is either empty or insecure"));
-    }
+                    throw(new \InvalidArgumentException("topic name is either empty or insecure"));
+                }
     }
 
             //verify the topic name length will fit in the database
@@ -65,10 +67,13 @@ class Topic
         // convert and store the title id
         $this->titleId = $uuid;
     }
-    /**
-     *accessor method for topic id
-     **/
-    public function getTopicId() : Uuid{
-            return($this->topicProfileId);
+        /**
+         *accessor method for topic id
+         **/
+        public
+        function getTopicId(): Uuid
+        {
+            return ($this->topicProfileId);
+        }
     }
 }
