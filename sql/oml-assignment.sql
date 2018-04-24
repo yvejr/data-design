34980795aee4c03a55dd1ee8644c2b9b44a98b3f
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-24 07:59:59 -0600

    added composer.lock

---
+++ sql/oml-assignment.sql
@@ -0,0 +1,21 @@
+INSERT INTO topic (topicId)
+VALUES
+
+UPDATE topic
+SET topicName = "Cows"
+WHERE
+
+DELETE FROM topic
+WHERE  = "Giraffes";
+
+INSERT INTO topic(topicId)
+VALUES
+
+UPDATE
+SET movieGenre = "Crime"
+WHERE movieGenre = "Thriller";
+
+DELETE FROM
+WHERE
+
+SELECT * FROM
